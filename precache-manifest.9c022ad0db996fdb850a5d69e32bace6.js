016a0cc2a084ef11149b7896b6e25187e18cadea
LAPTOP-IKDU3139\ADMIN <hunglt5@vng.com.vn>
2020-09-05 01:42:39 +0700

    Updates

---
+++ precache-manifest.9c022ad0db996fdb850a5d69e32bace6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa12d83ca29014265877dbe18d71003",
+    "url": "/management-work/index.html"
+  },
+  {
+    "revision": "31e0dd6f3d25e7937532",
+    "url": "/management-work/static/css/main.8955d074.chunk.css"
+  },
+  {
+    "revision": "87834b7474407c62bdc4",
+    "url": "/management-work/static/js/2.658a40c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/management-work/static/js/2.658a40c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e0dd6f3d25e7937532",
+    "url": "/management-work/static/js/main.9cf4b996.chunk.js"
+  },
+  {
+    "revision": "5b37181800e864657e29",
+    "url": "/management-work/static/js/runtime-main.f4d5c709.js"
+  }
+]);
